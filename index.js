3c8ba773c1cd1b80f5a05a9981f153b522ff73d3
Moe Hanafe <45694343+MoeHanafe@users.noreply.github.com>
2019-02-11 14:12:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//console.log("Hello from JavaScript");
+
+//data types;
+//var 
+//let
+//const 
+
+//var name = 'Moe';
+// let name = "Moe";
+//let Name = 'hanafe';
+
+//strings
+//let ex = 'string';
+//let ex2 = 'string';
+//console.log(ex+' 'ex2);
+//console.log`${ex} ${ex2}`;
+
+//numbers
+//let num = 1;
+//let num2 = 1.11111;
+//console.log(parlseFloat(num2))
+//console.log(parseInt(num2))
+//console.log(num2. toFixed)
+ 
+ //values
+//let ex = true;
+//let ex2 = false;
+// let ex3 = null;
+//let ex4 = -2;
+//let ex5 = undefiened;
+//let example = NaN;
+//console.log(Boolean(ex));
+
+//arrays
+//let ex = [4, 2 ,3];
+//ex.push(1, 2, 3);
+//ex[0] = 1;
+//ex.pop();
+//console.log(ex);
+//ex.forEach((element) => {console.log(element)});
